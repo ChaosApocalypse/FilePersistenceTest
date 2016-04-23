5ce7bd9cdecdbcc73e92475beb83c358aa26c8fd
lidian <02akaba@gmail.com>
2016-04-23 20:37:41 +0800

    文件输出测试

---
+++ app/src/main/java/com/example/ddd/filepersistencetest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.ddd.filepersistencetest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit = (EditText) findViewById(R.id.edit);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = edit.getText().toString();
+        save (inputText);
+    }
+
+    public void save(String inputText) {
+        FileOutputStream fileOutputStream = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileOutputStream = openFileOutput("data", Context.MODE_PRIVATE);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            bufferedWriter.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
